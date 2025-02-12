1dfc9e65f8441a1b1414ba9c9dd8f6f703c940b0
Tariq <alskrantariq@gmail.com>
2025-02-12 23:16:25 +0300

    Components

--- src/components/BaseIcon/icons.ts
+++
@@ -1,8 +0,0 @@
-export { default as chevron_down } from './icons/chevron-down';
-export { default as chevron_left } from './icons/chevron-left';
-export { default as chevron_right } from './icons/chevron-right';
-export { default as chevron_up } from './icons/chevron-up';
-export { default as close } from './icons/close';
-export { default as arrow_left } from './icons/arrow-left';
-export { default as big_warning } from './icons/big-warning';
-export { default as check_success } from './icons/check-success';
